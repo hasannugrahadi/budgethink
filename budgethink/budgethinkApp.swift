d0a6ec008edd2af9bd1c5e5c0d293a9a57ff4c13
hasannugrahadi <hasannugrahadi03@gmail.com>
2025-05-13 23:24:31 +0700

    Initial Commit

---
+++ budgethink/budgethinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgethinkApp.swift
+//  budgethink
+//
+//  Created by MacBook on 13/05/25.
+//
+
+import SwiftUI
+
+@main
+struct budgethinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
